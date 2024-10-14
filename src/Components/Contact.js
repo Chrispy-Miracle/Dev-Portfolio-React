c72e09903ed6d2dbb7800be0e61f0af28f7d93f6
Chris Patchett <chrispymiracle@gmail.com>
2024-10-14 14:16:13 -0600

    minor contact section css improvements

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,19 +3,23 @@ import ContactLink from "./ContactLink";
 
 const Contact = () => {
     return (
-        <section id="Contact">
-            <ContactLink 
-                id="email"
-                href="mailto:chrispymiracle@gmail.com"
-                alt="my gmail"
-                tooltip="CLICK TO SEND ME AN EMAIL!"
-                text="Contact me"
-            />
-            <a id="resume" className="links" href="./images/CPatchett-Dev-10-2024.pdf" 
-            target="_blank" rel="noreferrer" alt="link to my resume" title="CLICK TO SEE MY RESUME!">See my resume!</a>
-            <a id="github-profile-link" className="links" href="https://github.com/Chrispy-Miracle" 
-            target="_blank" rel="noreferrer" alt="my github profile" title="CLICK TO CHECK OUT MY GITHUB">Let's Work together</a>
-        </section>
+        <div>
+            <h2>Thanks so much for checking out my portfolio!</h2>
+            <section id="Contact">
+                <ContactLink 
+                    id="email"
+                    href="mailto:chrispymiracle@gmail.com"
+                    alt="my gmail"
+                    tooltip="CLICK TO SEND ME AN EMAIL!"
+                    text="Contact me"
+                />
+                <a id="resume" className="links" href="./images/CPatchett-Dev-10-2024.pdf" 
+                target="_blank" rel="noreferrer" alt="link to my resume" title="CLICK TO SEE MY RESUME!">See my resume!</a>
+                <a id="github-profile-link" className="links" href="https://github.com/Chrispy-Miracle" 
+                target="_blank" rel="noreferrer" alt="my github profile" title="CLICK TO CHECK OUT MY GITHUB">Let's Work together</a>
+            </section>            
+        </div>
+
     )
 }
 
